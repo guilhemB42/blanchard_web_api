93cee62e680ffe4fb5f5d262a85306fcf43ed37c
guilhèm blanchard <github@gblanchard.eu>
2024-01-15 22:22:03 +0100

    fix : put ok

--- blanchard_web_api/Controllers/TrackController.cs
+++ blanchard_web_api/Controllers/TrackController.cs
@@ -36,5 +36,13 @@ namespace blanchard_web_api.Controllers
             Track track = new() {Title = title, ArtistName = artist, DurationInSecond = duration };
             return Ok(await trackRepository.Post(track));
         }
+        [HttpPut("Put/{id}/Title/{title}/Artist/{artist}/Duration/{duration}")]
+        public async Task<IActionResult> Put(int id, string title, string artist, int duration) {
+            Track track = await trackRepository.GetByIdAsync(id);
+            track.Title = title;
+            track.ArtistName = artist;
+            track.DurationInSecond = duration;
+            return Ok(await trackRepository.Put(track));
+        }
     }
 }
