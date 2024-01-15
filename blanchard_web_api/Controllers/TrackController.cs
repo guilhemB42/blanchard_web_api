8046435598a6702d66945d914c2c7892eeaf0ec5
guilhèm blanchard <github@gblanchard.eu>
2024-01-15 17:21:55 +0100

    debut api

---
+++ blanchard_web_api/Controllers/TrackController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace blanchard_web_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TrackController : ControllerBase
+    {
+        ITrackRepository trackRepository;
+          private readonly ILogger<TrackController> _logger;
+
+        public TrackController(ILogger<TrackController> logger, ITrackRepository ItrackRepository)
+        {
+            trackRepository = ItrackRepository;
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetTrack")]
+        public IActionResult Get()
+        {
+            return Ok(trackRepository.GetAllAsync());           
+        }
+    }
+}
