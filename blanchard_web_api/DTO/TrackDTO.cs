f7adb92046b76c8b2f6e576c049f57eebfcff8e9
guilhèm blanchard <github@gblanchard.eu>
2024-01-17 12:56:32 +0100

    fix : dto track avec clé etrangère

--- blanchard_web_api/DTO/TrackDTO.cs
+++ blanchard_web_api/DTO/TrackDTO.cs
@@ -1,4 +1,7 @@
-﻿namespace blanchard_web_api.DTO
+﻿using blanchard_web_api.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace blanchard_web_api.DTO
 {
     public class TrackDTO
 
@@ -6,5 +9,8 @@
         public string Title { get; set; }
         public string ArtistName { get; set; }
         public int DurationInSecond { get; set; }
+        [ForeignKey("Artist")]
+        public int ArtistId { get; set; }
+        public Artist? Artist { get; set; }
     }
 }
