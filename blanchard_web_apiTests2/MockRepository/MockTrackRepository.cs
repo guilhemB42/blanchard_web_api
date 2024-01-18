d2afd72267bd072ffb475589b9f6b3860b51e5cc
guilhèm blanchard <github@gblanchard.eu>
2024-01-18 23:26:43 +0100

    add : test unitaire

---
+++ blanchard_web_apiTests2/MockRepository/MockTrackRepository.cs
@@ -0,0 +1,70 @@
+﻿using blanchard_web_api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using blanchard_web_api;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blanchard_web_apiTests2.MockRepository
+{
+    internal class MockTrackRepository : ITrackRepository
+    {
+        Context context;
+        public MockTrackRepository(Context context)
+        {
+            this.context = context;
+        }
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Track>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Track> GetAll()
+        {
+            return new List<Track>() { new Track { Id = 17 } };
+        }
+
+        public Task<Track> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Track> PatchArtist(Track track)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Track> PatchDuration(Track track)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Track> PatchTitle(Track track)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Track> Post(Track track)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Track> Put(Track track)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Track>> SearchAsync(string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ blanchard_web_apiTests2/TrackRepositoryTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using blanchard_web_api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using blanchard_web_api.Entities;
+using blanchard_web_apiTests2.MockRepository;
+
+namespace blanchard_web_api.Tests
+{
+    [TestClass()]
+    public class TrackRepositoryTests
+    {
+        [TestMethod()]
+        public void GetAllAsyncTest()
+        {
+            // Arrange
+            var builder = new DbContextOptionsBuilder<Context>().UseInMemoryDatabase("music");
+
+
+            var context = new Context(builder.Options);
+            context.Database.EnsureDeleted();
+/*          context.Tracks.Add(new Track { Id = 343, ArtistName="aaa", Title="bbb",DurationInSecond=15 });
+            context.Tracks.Add(new Track { Id = 25, ArtistName="bbb", Title="ccc",DurationInSecond=65 });
+*/
+
+            MockTrackRepository petRepository = new MockTrackRepository(context);
+
+            // Act
+            var tracks = petRepository.GetAll();
+            Track track = tracks.ElementAt(0);
+
+            // Assert
+            Assert.AreEqual(track.Id, 17);
+        }
+    }
+}
