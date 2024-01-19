c0f4ad22609d9599ce27e216a2777c803b0782af
guilhèm blanchard <github@gblanchard.eu>
2024-01-19 11:10:59 +0100

    fix : typo

--- blanchard_web_apiTests2/TrackRepositoryTests.cs
+++ blanchard_web_apiTests2/TrackRepositoryTests.cs
@@ -1,13 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using blanchard_web_api;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using blanchard_web_api.Entities;
-using blanchard_web_apiTests2.MockRepository;
 
 namespace blanchard_web_api.Tests
 {
@@ -34,10 +27,10 @@ namespace blanchard_web_api.Tests
             ArtistRepository artistRepo = new ArtistRepository(context);
 
             // Act
-            var artist = await artistRepo.GetAllAsync();
+            var artists = await artistRepo.GetAllAsync();
 
             // Assert
-            Assert.AreEqual(105, artist.First().Id);
+            Assert.AreEqual(105, artists.First().Id);
         }
     }
 }
